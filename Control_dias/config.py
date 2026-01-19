33226c49bbb29e5f15443e6e3ba80b31acce4d12
CarlosAvilesTapia <cavilest@gmail.com>
2026-01-19 15:47:12 -0300

    se saca config.py del control de versiones

--- Control_dias/config.py
+++
@@ -1,9 +0,0 @@
-import os
-
-class Config:
-    SECRET_KEY = os.getenv('SECRET_KEY', 'clave_por_defecto')
-    DATABASE = os.path.join(os.path.dirname(__file__), 'mi_base_PROD_2025.db')
-    
-    # Módulos de horas y compensadas apagados
-    FEATURE_HORAS_EXTRAS = False
-    FEATURE_COMPENSADAS = False
